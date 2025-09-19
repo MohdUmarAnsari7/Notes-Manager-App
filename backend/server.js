868379cda92081e5bc5ea8265d6c3a1fc2e89cfd
My Name <umaransari4444r@gmail.com>
2025-09-19 16:59:22 +0530

    com

--- backend/server.js
+++ backend/server.js
@@ -45,8 +45,8 @@ if (isProduction) {
   const path = require('path');
   app.use(express.static(path.join(__dirname, '../frontend/build')));
   
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
   });
 } else {
   app.get('/', (req, res) => {
